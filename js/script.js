77d65a270cc5a923fef733613479b1fec580eed5
Gustav Emrich <gustavjemrich@gmail.com>
2024-03-12 10:22:02 +0100

    la till contact sida och desktop för main.

--- js/script.js
+++ js/script.js
@@ -10,4 +10,20 @@ function showMenu( ){
 
     let layer = document.querySelector(".fade-layer");
     layer.classList.toggle("visible")
+
+    
 }
+
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var message = document.getElementById('message').value;
+    
+    // You can implement further validation here before sending the data
+    
+    // Here you can implement code to send the form data to your server using AJAX
+    
+    document.getElementById('response').innerHTML = '<p>Thank you, ' + name + ', your message has been sent!</p>';
+});
