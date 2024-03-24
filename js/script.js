ed34dd8ac9f3b43ab0a9c03cc81bf77c59b6b398
gustavemrich <gustavjemrich@gmail.com>
2024-03-24 20:49:40 +0100

    final touches

--- js/script.js
+++ js/script.js
@@ -4,26 +4,22 @@ document.querySelector(".menu-button").addEventListener("click", showMenu)
 
 
 
-function showMenu( ){
+function showMenu() {
     let menu = document.querySelector("nav.menu");
     menu.classList.toggle("show");
 
     let layer = document.querySelector(".fade-layer");
     layer.classList.toggle("visible")
 
-    
+
 }
 
-document.getElementById('contactForm').addEventListener('submit', function(event) {
+document.getElementById('contactForm').addEventListener('submit', function (event) {
     event.preventDefault();
-    
+
     var name = document.getElementById('name').value;
     var email = document.getElementById('email').value;
     var message = document.getElementById('message').value;
-    
-    // You can implement further validation here before sending the data
-    
-    // Here you can implement code to send the form data to your server using AJAX
-    
+
     document.getElementById('response').innerHTML = '<p>Thank you, ' + name + ', your message has been sent!</p>';
 });
